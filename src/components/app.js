424e838ec7de6e8acca1da4a7da4c4292845dc2b
rtmcchesney <rtyler.mcchesney@gmail.com>
2019-10-03 22:38:46 -0600

    Added Nav Components

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,18 @@
-import React, { Component } from 'react';
-import moment from "moment"
+  
+import React, { Component } from "react";
+import moment from "moment";
 
+import PortfolioContainer from "./portfolio/portfolio-container";
+import NavigationContainer from "./navigation/navigation-container";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>Tyler McChesney Portfolio</h1>
-        <div>
-          {moment().format('MMMM Do YYYY, h:mm:ss a')}
-        </div>
+      <div className="app">
+        <NavigationContainer />
+        <h1>Jordan Hudgens Portfolio</h1>
+        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
+        <PortfolioContainer />
       </div>
     );
   }
